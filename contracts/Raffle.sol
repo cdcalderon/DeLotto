36baa46181d4c32554e21541bca13c7172e27402
Carlos Calderon <cdcalderon@gmail.com>
2022-06-17 19:14:59 -0500

    add VRFCoordinatorV2Interface and call requestRandomWords

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,19 +2,37 @@
 
 pragma solidity ^0.8.17;
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughEthEntered();
 
 contract Raffle is VRFConsumerBaseV2 {
     /* State variables */
     uint256 private immutable i_entranceFee;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     address payable[] private s_players;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_entranceFee = entranceFee;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -33,7 +51,15 @@ contract Raffle is VRFConsumerBaseV2 {
         override
     {}
 
-    function requestRandomWinner() external {}
+    function requestRandomWinner() external {
+        i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
 
     /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
