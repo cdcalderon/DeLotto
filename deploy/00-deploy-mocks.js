079b3687b78be7c9c4b474fe568f64f75aebd447
Carlos Calderon <cdcalderon@gmail.com>
2022-06-22 12:04:57 -0500

    implement code to deploy VRFCoordinatorV2Mock

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,6 +1,9 @@
 const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 
+const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
+const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
@@ -8,6 +11,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
+
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        })
+        log("Mocks Deployed!")
+        log("----------------------------------------------------------")
     }
 }
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -49,21 +49,21 @@ module.exports = {
             saveDeployments: true,
             chainId: 5,
         },
-        mainnet: {
-            url: MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
-            saveDeployments: true,
-            chainId: 1,
-        },
-        polygon: {
-            url: POLYGON_MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            saveDeployments: true,
-            chainId: 137,
-        },
+        // mainnet: {
+        //     url: MAINNET_RPC_URL,
+        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        //     //   accounts: {
+        //     //     mnemonic: MNEMONIC,
+        //     //   },
+        //     saveDeployments: true,
+        //     chainId: 1,
+        // },
+        // polygon: {
+        //     url: POLYGON_MAINNET_RPC_URL,
+        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        //     saveDeployments: true,
+        //     chainId: 137,
+        // },
     },
     solidity: {
         compilers: [
