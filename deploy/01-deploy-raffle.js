a2b4b16222018b8354373bf2b9a072d6b5bc379b
Carlos Calderon <cdcalderon@gmail.com>
2022-06-22 15:57:46 -0500

    add missing arguments

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,7 +1,22 @@
+const { ethers } = require("ethers")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const arguments = [vrfCoordinatorV2Address, entranceFee, gasLane]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: [],
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,6 +2,13 @@ const networkConfig = {
     5: {
         name: "goerli",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // anything doesn't matter 30 gwei
     },
 }
 
