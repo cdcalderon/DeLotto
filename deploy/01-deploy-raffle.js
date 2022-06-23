ec6b0870189094ee2c319c0c47c61aa45012590f
Carlos Calderon <cdcalderon@gmail.com>
2022-06-22 19:26:53 -0500

    create subscription

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,17 +1,25 @@
 const { ethers } = require("ethers")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
+const FUND_AMOUNT = ethers.utils.parseEther("1") // 1 ETH or 1^18
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address
+    let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
